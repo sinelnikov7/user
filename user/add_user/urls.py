56c6e4c393f6db39e55b79b3edc3a7c9f7ff56df
sinelnikov7 <sinelnikovby@gmail.com>
2022-10-09 10:01:45 +0300

    Добавлена логика удаления

--- user/add_user/urls.py
+++ user/add_user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import add_user, user_detail
+from .views import add_user, user_detail, users_delete, all_users
 
 app_name = 'user'
 
 urlpatterns = [
     path('add_user/', add_user, name='add_user'),
-    path('user_detail/<int:pk>/', user_detail, name='user_detail')
+    path('user_detail/delete/', users_delete, name='users_delete'),
+    path('user_detail/all_users/', all_users, name='all_users'),
+    path('user_detail/<int:pk>/', user_detail, name='user_detail'),
 ]
--- user/add_user/views.py
+++ user/add_user/views.py
@@ -1,28 +1,58 @@
-from django.shortcuts import render
-
+import sqlite3
 import requests
+
+from django.shortcuts import render, redirect
 
 from .models import User
 
 
 def add_user(request):
-    users = User.objects.all()
-    context = {
-        'users': users
-    }
+
+    last_user = User.objects.all().last()
+    slots = ''
+    need_button_add_user = True
+    if last_user == None:
+        need_buttons_delete_and_all_users = False
+    else:
+        need_buttons_delete_and_all_users = True
     if request.method == "POST":
-        last_user = User.objects.all().last()
         if last_user == None:
             number = 1
         else:
             number = last_user.id + 1
         response = requests.get(f'https://jsonplaceholder.typicode.com/users/{number}')
-        name = response.json()['name']
-        username = response.json()['username']
-        phone = response.json()['phone']
-        city = response.json()['address']['city']
-        User.objects.create(name=name, username=username, phone=phone, city=city)
+        if response.status_code == 200:
+            name = response.json()['name']
+            username = response.json()['username']
+            phone = response.json()['phone']
+            city = response.json()['address']['city']
+            User.objects.create(name=name, username=username, phone=phone, city=city)
+            need_buttons_delete_and_all_users = True
+        if response.status_code == 404:
+            slots = 'Больше нет данных для добовления'
+            need_button_add_user = False
+    context = {
+        'slots': slots,
+        'need_buttons_delete_and_all_users': need_buttons_delete_and_all_users,
+        'need_button_add_user': need_button_add_user
+    }
     return render(request, 'add_user.html', context)
+
+
+def users_delete(request):
+    connect = sqlite3.connect('db.sqlite3')
+    cursor = connect.cursor()
+    cursor.execute('DELETE FROM add_user_user')
+    cursor.execute('delete from sqlite_sequence where name="add_user_user"')
+    connect.commit()
+    cursor.close()
+    return redirect('user:add_user')
+
+def all_users(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'all_users.html', context)
 
 def user_detail(request, pk):
     user = User.objects.get(id=pk)
